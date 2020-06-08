b647c1c4f1748b655ae4a27034e216e677b40621
Machigai <grunin200412@gmail.com>
2020-06-08 23:13:42 +0300

    worked

--- data/script.js
+++ data/script.js
@@ -1,17 +1,23 @@
 //home;man;woman;news;contacts
 let links =[
   "https://github.com/FatalErrror/trandyHall/raw/master/data/main1.jpg", 
-  "",
-  "https://github.com/FatalErrror/trandyHall/raw/master/data/main.jpg",
-  "https://github.com/FatalErrror/trandyHall/raw/master/data/home.png",
-  ""
+  "https://github.com/FatalErrror/trandyHall/raw/master/data/%D1%8C%D1%84%D1%82.png",
+  "http://7sp.ru/files/6bf/6bfcaa268556de3f2228104cd13a6d48-fit-400x400.jpg",
+  "https://github.com/FatalErrror/Server/raw/master/Task.png",
+  "https://github.com/FatalErrror/Server/raw/master/NewMap.png"
 ]
 
-let button = document.querySelector('.button');
-let image = document.querySelector('.image');
+var button = document.getElementsByTagName('button');
+let content = document.querySelector('.content');
+console.log(button);
+// при клике по кнопке скрипт начинает выбирать
 
 for(let i = 0; i < button.length; i = i + 1){
-  button[i].addEventListener('click', function () {
-    image.src = links[i];
-});
+  button[i].onclick = function() {
+    content.src = links[i];
+  };
 }
+console.log(button);
+
+
+
